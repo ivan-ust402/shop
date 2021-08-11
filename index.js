338b73320a8e4f876250cc77d5639945ba120c05
Ivan Ustyantsev <vangold915@gmail.com>
2021-08-11 12:30:35 +0700

    add comments

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express')
 //Для работы с путями в ОС
 const path = require('path')
-const exphbs = require('express-handlebars')
+const exphbs = require('express-handlebars') //подключаем пакет hbrs
 const homeRoutes = require('./routes/home')
 const cardRoutes = require('./routes/card')
 const coursesRoutes = require('./routes/courses')
@@ -11,16 +11,19 @@ const addRoutes = require('./routes/add')
 //Создание срвера на экспрессе
 const app = express() //объект app аналог объекта сервер
 
+//создание объекта hbrs
 const hbs = exphbs.create({
-  defaultLayout: 'main',
+  defaultLayout: 'main', 
   extname: 'hbs'
 })
 
-app.engine('hbs', hbs.engine)
-app.set('view engine', 'hbs')
-app.set('views', 'views')
+//Регистрация движка в экспрессе для рендеринга html страниц
+app.engine('hbs', hbs.engine) //название, его значение
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.set('view engine', 'hbs') //с помощью метода set мы начинаем его использовать
+app.set('views', 'views') //название переменной views- ее мы конфигурируем, второй параметр название папки где хранятся шаблоны, по умолчанию это папка view, но здесь мы явно ее укажем
+
+app.use(express.static(path.join(__dirname, 'public'))) //делаем папку public статической для того, чтобы когда экспресс будет подгружать файлы с адресом /, он смотрит именно в нее
 app.use(express.urlencoded({extended: true}))
 
 app.use('/', homeRoutes)
